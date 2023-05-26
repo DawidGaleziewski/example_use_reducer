c5adc86897c1aa0df0fca43662cac363902b0fb0
Gałęziewski, Dawid <Dawid.Galeziewski@ergo.com>
2023-05-26 12:46:11 +0200

    update: basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,14 @@ function App() {
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div className="bg-white pt-7 pb-10 px-5 rounded w-96">
               <CloseIcon className="ml-auto mb-5 cursor-pointer" />
+              <div className="flex justify-between grow-1 mb-5">
+                <button className="bg-gray-400 px-2 py-3 rounded-l basis-1/3 border-r-2">Step 1</button>
+                <button className="bg-gray-400 px-2 py-3 basis-1/3 border-r-2">Step 2</button>
+                <button className="bg-gray-400 px-2 py-3 rounded-r basis-1/3">Step 3</button>
+              </div>
+
+              {/*STEP 1*/}
+              <h2 className="text-lg font-bold mb-5">Fill up the order</h2>
               <form className="flex flex-col">
                 <input className="p-2 mb-5 w-12/12 bg-gray-200 border-b-2 border-slate-700 rounded-t-sm" type="email" name="email" placeholder="your email" />
                 <select name="product" className="p-2 mb-5 bg-gray-200 border-b-2 border-slate-700 rounded-t-sm">
@@ -17,6 +25,15 @@ function App() {
                 <input name="quantity" placeholder="quantity" className="p-2 mb-5 bg-gray-200 border-b-2 border-slate-700 rounded-t-sm"/>
                 <input className="p-2 rounded-md bg-slate-700 text-white" type="submit" value="send" />
               </form>
+
+              {/*STEP 2 a*/}
+              {/*  <h2 className="text-lg font-bold mb-5">Specific step for category clothes</h2>*/}
+
+              {/*STEP 2 b*/}
+              {/*  <h2 className="text-lg font-bold mb-5">Specific step for category electronics</h2>*/}
+
+              {/*STEP 3*/}
+              {/*  <h2 className="text-lg font-bold mb-5">Summary</h2>*/}
             </div>
           </div>
         </div>
