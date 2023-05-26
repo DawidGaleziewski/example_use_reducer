3dce23100bff4f4a016190f32cc8f51c25511608
Gałęziewski, Dawid <Dawid.Galeziewski@ergo.com>
2023-05-26 12:02:53 +0200

    update: save basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {FC} from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div
-      className="relative z-10"
-      aria-labelledby="modal-title"
-      role="dialog"
-      aria-modal="true"
-    >
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <form className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              <input type="email" name="email" placeholder="your email" />
-              <select>
-                <option>Test 1</option>
-                <option>Test 2</option>
-              </select>
-              <input type="submit" value="send" />
-            </form>
+            <div className="bg-white pt-7 pb-10 px-5 rounded w-96">
+              <CloseIcon className="ml-auto mb-5 cursor-pointer" />
+              <form className="flex flex-col">
+                <input className="p-2 mb-5 w-12/12 bg-gray-200 border-b-2 border-slate-700 rounded-t-sm" type="email" name="email" placeholder="your email" />
+                <select name="product" className="p-2 mb-5 bg-gray-200 border-b-2 border-slate-700 rounded-t-sm">
+                  <option>Test 1</option>
+                  <option>Test 2</option>
+                </select>
+                <input name="quantity" placeholder="quantity" className="p-2 mb-5 bg-gray-200 border-b-2 border-slate-700 rounded-t-sm"/>
+                <input className="p-2 rounded-md bg-slate-700 text-white" type="submit" value="send" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
+const CloseIcon:FC<{className: string}> = (props) => <svg {...props} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="15px" height="15px"><path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"/></svg>
 export default App;
